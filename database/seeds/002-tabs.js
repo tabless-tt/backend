1cc334e2ca7e582bf9fd96e93eb9fd1badc690ee
Irving Duran <ijd.irving@gmail.com>
2019-05-20 14:48:03 -0300

    seeds and migrate changes

--- database/seeds/002-tabs.js
+++ database/seeds/002-tabs.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('tabs').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('tabs').insert([
         {
         userId: 1,  
         title: 'google', 
