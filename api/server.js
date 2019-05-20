8b5b72c8e2effaf0b3917007f5bfd8307d71a84e
Irving Duran <ijd.irving@gmail.com>
2019-05-20 17:17:19 -0300

    cors change

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
+const cors = require("cors");
 const express = require("express");
 const helmet = require("helmet");
-const cors = require("cors");
 
 const authRouter = require("../routers/auth/authRouter");
 const usersRouter = require("../routers/users/usersRouter");
-const tabsRouter = require('../routers/tabs/tabsRouter')
+const tabsRouter = require("../routers/tabs/tabsRouter");
 
 const server = express();
 
@@ -14,7 +14,7 @@ server.use(cors());
 
 server.use("/api/", authRouter);
 server.use("/api/users", usersRouter);
-server.use('/api/tabs', tabsRouter)
+server.use("/api/tabs", tabsRouter);
 
 server.get("/", (req, res) => {
   res.send("api running");
--- routers/users/usersRouter.js
+++ routers/users/usersRouter.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const Users = require("./usersModel");
+const Tabs = require("../tabs/tabsModel");
 const restricted = require("../../middleware/restrictedMiddleware");
 
 router.get("/", restricted, (req, res) => {
@@ -11,5 +12,21 @@ router.get("/", restricted, (req, res) => {
     .catch(err => res.send(err));
 });
 
-module.exports = router;
+router.get("/:id/", (req, res) => {
+  Users.findById(req.params.id)
+    .then(users => {
+      Tabs.find()
+        .where({ user_id: req.params.id })
+        .then(tabs => {
+          users.tabs = tabs;
+          return res.status(200).json(users);
+        });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "we ran into an error retreving the user" });
+    });
+});
 
+module.exports = router;
