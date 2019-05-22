d4e745f3174d348ef8e56fc675a324292ef508c4
Irving Duran <ijd.irving@gmail.com>
2019-05-22 18:19:12 -0300

    seeds

--- routers/auth/auth.spec.js
+++ routers/auth/auth.spec.js
@@ -3,15 +3,16 @@ const db = require("../../database/dbConfig");
 const Auth = require("./authModel.js");
 
 describe("auth model", () => {
+
   afterEach(async () => {
     await db("users").truncate();
   });
+
   describe("add()", () => {
+
     it("should insert new user", async () => {
       await Auth.add({ username: "irving", password: "irving" });
-
       const user = await db("users");
-
       expect(user).toHaveLength(1);
     });
 
@@ -26,6 +27,7 @@ describe("auth model", () => {
       expect(users).toHaveLength(2);
     });
   });
+
   describe("find()", () => {
     it("should return an array", async () => {
       const find = await Auth.find();
@@ -33,6 +35,7 @@ describe("auth model", () => {
       expect(find).toEqual(array);
     });
   });
+
   describe("add(), findById() together", () => {
     it("should return users info, email null", async () => {
       let user = await Auth.add({ username: "irving", password: "irving" });
@@ -40,18 +43,30 @@ describe("auth model", () => {
 
       const find = await Auth.findById(1);
       const array = [];
-      expect(find).toEqual({"email": null, "id": 1, "password": "irving", "username": "irving"});
+      expect(find).toEqual({
+        email: null,
+        id: 1,
+        password: "irving",
+        username: "irving"
+      });
     });
 
     it("should return users info", async () => {
-      let user = await Auth.add({ username: "irving", password: "irving", email: "irving@gmail.com" });
+      let user = await Auth.add({
+        username: "irving",
+        password: "irving",
+        email: "irving@gmail.com"
+      });
       // expect(user.username).toBe("irving");
 
       const find = await Auth.findById(1);
       const array = [];
-      expect(find).toEqual({"email": "irving@gmail.com", "id": 1, "password": "irving", "username": "irving"});
+      expect(find).toEqual({
+        email: "irving@gmail.com",
+        id: 1,
+        password: "irving",
+        username: "irving"
+      });
     });
-    });
-
-
+  });
 });
