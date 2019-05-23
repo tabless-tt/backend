d29379883e9668ec0e7349b2bd80ef3499a3f65e
Irving Duran <ijd.irving@gmail.com>
2019-05-23 19:56:46 -0300

    producttion changesd

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -2,12 +2,13 @@ const bcrypt = require('bcryptjs')
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('users')
-  .del()
+  // .del()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
         {username: 'lambda', password: bcrypt.hashSync('password', 10)},
         {username: 'michael', password: bcrypt.hashSync('password', 10)},
+        {username: 'irving', password: bcrypt.hashSync('password', 10)}
       ]);
     });
 };
