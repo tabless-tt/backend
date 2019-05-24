3d0120c89add476c81ca7b710d7141f5e5fe0f8a
Irving Duran <ijd.irving@gmail.com>
2019-05-24 09:57:23 -0300

    knexfile edit

--- knexfile.js
+++ knexfile.js
@@ -49,15 +49,22 @@ module.exports = {
       directory: "./database/seeds"
     }
   },
-  production2: {
-    client: "pg",
-    connection: `${process.env.DATABASE_URL}`,
+  productionSqlite: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database/tabless.db3"
+    },
     useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
     migrations: {
       directory: "./database/migrations"
     },
     seeds: {
       directory: "./database/seeds"
     }
-  }
+  },
 };
